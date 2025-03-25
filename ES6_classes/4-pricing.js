a1073dbadef21305c5f254062d24bd32d28ae9ae
seb25000 <seb25000@gmail.com>
2025-03-25 18:18:16 +0100

    4-pricing.js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,38 +1,39 @@
-import Currency from './3-currency';
 
-export default class Pricing {
+class Pricing {
   constructor(amount, currency) {
-    // Create objs
     this._amount = amount;
     this._currency = currency;
   }
 
-  // Methods
-
-  displayFullPrice() {
-    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
-  }
-
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
-
-  // Setters
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  set currency(newCurrency) {
-    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
-    this._currency = newCurrency;
-  }
-
-  // Getters
+  // Getter for amount
   get amount() {
     return this._amount;
   }
 
+  // Setter for amount
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter for currency
   get currency() {
     return this._currency;
   }
+
+  // Setter for currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
+
+export default Pricing;
