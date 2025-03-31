f20d9cfdf59dfb1f7e69f3562c8cfa46ab5b37f4
seb25000 <seb25000@gmail.com>
2025-03-31 15:31:08 +0200

    1-main.js

--- ES6_data_manipulation/1-main.js
+++ ES6_data_manipulation/1-main.js
@@ -3,8 +3,3 @@ import getListStudents from "./0-get_list_students.js";
 
 console.log(getListStudentIds("hello"));
 console.log(getListStudentIds(getListStudents()));
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 1-main.js 
-[]
-[ 1, 2, 5 ]
